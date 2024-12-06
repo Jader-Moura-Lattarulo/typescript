86e33e76469a86614544d9291674194a4f2d5294
Jader Moura <jader.dev.moura@gmail.com>
2024-12-06 15:38:28 -0300

    exercicios de TS

--- build/index.js
+++ build/index.js
@@ -1,6 +1,47 @@
 "use strict";
-const paciente = {
-    nome: "Jader",
-    idade: "35"
+let number = 245;
+let word = "palavra";
+console.log(number, word);
+console.log("----------------------------");
+let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(numbersArray[0]);
+console.log("----------------------------");
+const person = {
+    name: "Jader",
+    age: 35
 };
-console.log(paciente);
+console.log(person);
+console.log("----------------------------");
+function sumNumbers(number01, number02) {
+    return number01 + number02;
+}
+console.log(sumNumbers(5, 4));
+console.log("----------------------------");
+class Bmw {
+    constructor(brand, year) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
+const bmw01 = new Bmw("BMW", 2024);
+console.log(bmw01);
+console.log("----------------------------");
+class MotocicleClass {
+    constructor(tipo, cylinderCapacity) {
+        this.tipo = tipo;
+        this.cylinderCapacity = cylinderCapacity;
+    }
+}
+const motocicle01 = new MotocicleClass("Esportiva", 1000);
+console.log(motocicle01);
+console.log("----------------------------");
+let numberOrString;
+numberOrString = "Jader";
+numberOrString = 35;
+console.log(typeof (numberOrString));
+console.log("----------------------------");
+function printFirstElement(array) {
+    console.log(array[1]);
+}
+printFirstElement(["Jader", 24, "Marly", "Laura"]);
+console.log("----------------------------");
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,89 @@
-const paciente: {
-    nome: string,
-    idade: string
+let number: number = 245;
+let word: string = "palavra"
+
+console.log(number, word);
+console.log("----------------------------");
+
+let numbersArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(numbersArray[0]);
+console.log("----------------------------");
+
+const person: {
+    name: string,
+    age: number
 } = {
-    nome: "Jader",
-    idade: "35"
+    name: "Jader",
+    age: 35
 }
 
-console.log(paciente);
+console.log(person);
+console.log("----------------------------");
+
+function sumNumbers (number01: number, number02: number): number {
+    return number01 + number02;
+}
+
+console.log(sumNumbers(5, 4)); 
+
+console.log("----------------------------");
+
+interface Car {
+    brand: string;
+    year: number;
+}
+
+class Bmw implements Car {
+    brand: string,
+    year: number;
+
+    constructor(brand: string, year:number) {
+        this.brand = brand;
+        this.year = year;
+    }
+}
+
+const bmw01 = new Bmw("BMW", 2024);
+
+console.log(bmw01);
+
+
+console.log("----------------------------");
+
+interface Vehicle {
+    tipo: string;
+}
+
+interface Motocicle extends Vehicle {
+    cylinderCapacity: number;
+}
+
+class MotocicleClass implements Motocicle {
+    tipo: string;
+    cylinderCapacity: number;
+
+    constructor(tipo: string, cylinderCapacity: number) {
+        this.tipo = tipo;
+        this.cylinderCapacity = cylinderCapacity;
+    }
+}
+
+const motocicle01 = new MotocicleClass("Esportiva", 1000);
+console.log(motocicle01);
+
+console.log("----------------------------");
+
+let numberOrString: string | number;
+numberOrString = "Jader"
+numberOrString = 35
+console.log(typeof(numberOrString));
+
+
+console.log("----------------------------");
+
+function printFirstElement<T>(array: T[]): void {
+        console.log(array[1]);        
+}
+
+printFirstElement(["Jader", "Gabi", "Marly", "Laura"])
+
+console.log("----------------------------");
