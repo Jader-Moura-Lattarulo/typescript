65042f4814842c8bb346ecbf9a8337a17aad184c
Jader Moura <jader.dev.moura@gmail.com>
2024-12-06 12:14:33 -0300

    criando gitignore

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const paciente = {
+    nome: "Jader",
+    idade: "35"
+};
+console.log(paciente);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const paciente: {
+    nome: string,
+    idade: string
+} = {
+    nome: "Jader",
+    idade: "35"
+}
+
+console.log(paciente);
